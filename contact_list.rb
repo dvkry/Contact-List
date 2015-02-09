96a04ef0264ffda1928e3729b0391425db399725
David Vickery <david.vickery.23@gmail.com>
2015-02-09 22:02:29 +0000

    created Application class

--- contact_list.rb
+++ contact_list.rb
@@ -1,16 +1,23 @@
 require_relative 'contact'
 require_relative 'contact_database'
 
-def show_help
-  puts "Here is a list of available commands:"
-  puts "  new  - Create a new contact"
-  puts "  list - List all contacts"
-  puts "  show - Show a contact"
-  puts "  find - Find a contact"
+class Application
+
+  def show_help
+    puts "Here is a list of available commands:"
+    puts "  new  - Create a new contact"
+    puts "  list - List all contacts"
+    puts "  show - Show a contact"
+    puts "  find - Find a contact"
+  end
+
+  def command(arg)
+    case arg
+    when 'help'
+      show_help
+    end
+  end
 end
 
-case ARGV.first
-when 'help'
-  show_help
-end
-
+app = Application.new
+app.command(ARGV.first)
