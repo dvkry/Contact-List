af42ea33e1725526e895fe5fb5ae06eef2b5f119
David Vickery <david.vickery.23@gmail.com>
2015-02-11 01:34:21 +0000

    refactored all commands into their own Classes

--- contact_list.rb
+++ contact_list.rb
@@ -4,26 +4,39 @@ require 'pry'
 require 'io/console'
 
 class Application
+  def command(arg)
+    case arg
+    when 'help'
+      Help.run
+    when 'new'
+      id = CreateNew.run
+    when 'list'
+      List.run
+    when 'find'
+      Find.run(ARGV[1])
+    end
+  end
+end
 
-  def show_help
+class Help
+  def self.run
     puts "Here is a list of available commands:"
     puts "  new  - Create a new contact"
     puts "  list - List all contacts"
     puts "  show - Show a contact"
     puts "  find - Find a contact"
   end
+end
 
-  def create_new
-
+class CreateNew
+  def self.run
     print "E-mail: "
     email = STDIN.gets.chomp
-
     if Contact.dups_by_email(email)
       puts "That E-mail already exists"
     else
       print "Name: "
       name = STDIN.gets.chomp
-
       print "Would you like to add the phone numbers?"
       response = STDIN.gets.chomp.downcase
       phone_hash = {}
@@ -36,34 +49,32 @@ class Application
         print "add another number? "
         response = STDIN.gets.chomp.downcase
       end
-
       id = Contact.create(name, email, phone_hash) # ID is the line number
       puts id #return id
     end
   end
+end
 
-
-  def list
+class List
+  def self.wait_for_key
+    print "any key to continue"
+    STDIN.getch
+  end
+  def self.run
     contact_list = ContactDatabase.new.read_from_file
-
     id_counter = 1
-
     contact_list.each do |contact|
       puts id_counter.to_s + ": " + contact.name + " (" + contact.email.strip + ")"
       puts contact.phone_nums if contact.phone_nums != {}
       id_counter += 1 # we know that each contact is on it's 'id' line
-      wait_for_key if id_counter % 5 == 0
+      wait_for_key if (id_counter - 1) % 5 == 0
     end
   end
+end
 
-  def wait_for_key
-    print "any key to continue"
-    STDIN.getch
-  end
-
-  def find(id)
+class Find
+  def self.run(id)
     found = Contact.find(id)
-
     if found != []
       found.each do |contact|
         puts contact.name
@@ -72,23 +83,8 @@ class Application
     else
       puts "Not found"
     end
-
-  end
-
-  def command(arg)
-    case arg
-    when 'help'
-      show_help
-    when 'new'
-      id = create_new
-    when 'list'
-      list
-    when 'find'
-      find(ARGV[1])
-    end
   end
 end
 
-# TODO read file
 app = Application.new
 app.command(ARGV.first)
